aa9483a07a64387f5f88eacd71a59a22fd14a2b9
Захаров Артём <zaharov.artyom.04@gmail.com>
2024-11-14 22:47:00 +0700

    lab5py

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, session, request
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
+from lab5 import lab5
+
+
+
 
 app = Flask(__name__)
+
+app.secret_key = 'секретно-секретный секрет'
+
+
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
+app.register_blueprint(lab5)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route("/")
 @app.route("/index")
@@ -15,26 +33,27 @@ def start():
 
 @app.route("/menu")
 def menu():
-        return """
+    return """
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список Лабораторных
-        </header>
+<head>
+    <title>НГТУ, ФБ, Лабораторные работы</title>
+</head>
+<body>
+    <header>
+        НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+    </header>
 
-        <a href="/lab1">Первая лабораторная</a> <br>
-        <a href="/lab2">Вторая лабораторная</a> <br>
-        <a href="/lab3">Третья лабораторная</a> <br>
-        
-
-        <footer>
-            &copy; Артём Захаров, ФБИ-23, 3 курс, 2024
-        </footer>
-    </body>
+    <h1>web-сервер на flask</h1>
+    <li> <a href="/lab1"> Лабораторная работа 1 </a></li>
+    <li> <a href="/lab2"> Лабораторная работа 2 </a></li>
+    <li> <a href="/lab3"> Лабораторная работа 3 </a></li>
+    <li> <a href="/lab4"> Лабораторная работа 4 </a></li>
+    <li> <a href="/lab5"> Лабораторная работа 5 </a></li>
+    <footer>
+        &copy; Артём Захаров, ФБИ-23, 3 курс, 2024
+    </footer>
+</body>
 </html>
 """
 
